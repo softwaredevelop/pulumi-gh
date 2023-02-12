5a6d2334d3b94f82d727a7f911dc76be1ab45232
softwaredevelop <61334390+softwaredevelop@users.noreply.github.com>
2023-02-12 12:27:57 +0100

    test: Add iac files

---
+++ iac/main.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"github.com/pulumi/pulumi-github/sdk/v5/go/github"
+	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+)
+
+func createRepository(ctx *pulumi.Context, name string) (*github.Repository, error) {
+	repository, err := github.NewRepository(ctx, name, &github.RepositoryArgs{
+		DeleteBranchOnMerge: pulumi.Bool(true),
+		Description:         pulumi.String("This is a test repository for Pulumi repository creation"),
+		HasIssues:           pulumi.Bool(true),
+		HasProjects:         pulumi.Bool(true),
+		Name:                pulumi.String(name),
+		Topics:              pulumi.StringArray{pulumi.String("pulumi"), pulumi.String("github"), pulumi.String("repository"), pulumi.String("test")},
+		Visibility:          pulumi.String("public"),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return repository, nil
+}
+
+func createBranchProtection(ctx *pulumi.Context, name string, repository *github.Repository) (*github.BranchProtection, error) {
+	branchProtection, err := github.NewBranchProtection(ctx, name, &github.BranchProtectionArgs{
+		RepositoryId:          repository.NodeId,
+		Pattern:               pulumi.String("main"),
+		RequiredLinearHistory: pulumi.Bool(true),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return branchProtection, nil
+}
+
+func main() {
+
+	pulumi.Run(func(ctx *pulumi.Context) error {
+
+		repository, err := createRepository(ctx, "pulumi-gh")
+		if err != nil {
+			return err
+		}
+
+		_, err = github.NewActionsSecret(ctx, "actionSecret", &github.ActionsSecretArgs{
+			Repository: repository.Name,
+			SecretName: pulumi.String("PULUMI_CONFIG_PASSPHRASE"),
+		})
+		if err != nil {
+			return err
+		}
+
+		_, err = createBranchProtection(ctx, "branchProtection", repository)
+		if err != nil {
+			return err
+		}
+
+		issueLabel1, err := github.NewIssueLabel(ctx, "issueLabel1", &github.IssueLabelArgs{
+			Color:       pulumi.String("E66E01"),
+			Description: pulumi.String("This issue is related to github-actions dependencies"),
+			Name:        pulumi.String("github-actions dependencies"),
+			Repository:  repository.Name,
+		})
+		if err != nil {
+			return err
+		}
+
+		issueLabel2, err := github.NewIssueLabel(ctx, "issueLabel2", &github.IssueLabelArgs{
+			Color:       pulumi.String("0A4DFE"),
+			Description: pulumi.String("This issue is related to docker dependencies"),
+			Name:        pulumi.String("docker dependencies"),
+			Repository:  repository.Name,
+		})
+		if err != nil {
+			return err
+		}
+
+		ctx.Export("repository", repository.Name)
+		ctx.Export("repositoryUrl", repository.HtmlUrl)
+		ctx.Export("issueLabel1", issueLabel1.Name)
+		ctx.Export("issueLabel2", issueLabel2.Name)
+
+		return nil
+	})
+
+}
